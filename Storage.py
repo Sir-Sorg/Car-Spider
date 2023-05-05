b5c03cb4c77d941089ee0af1621d46f81195d31f
Sir-Sorg <sinaorojlo53@gmail.com>
2023-05-05 11:37:17 +0330

    Storage file finish for saving data

--- Storage.py
+++ Storage.py
@@ -1,8 +1,5 @@
 import sqlite3
 
-connection = sqlite3.connect("cars_db.sqlite")
-cursor = connection.cursor()
-
 
 def preProcess(data: list):
     dataSet = list()
@@ -12,19 +9,32 @@ def preProcess(data: list):
         if len(info['price']['price']) <= 2:
             continue
         car = {'COLOR': info['detail']['body_color'],
-               'KM_RUNNING': info['detail']['mileage'],
+               'KM_RUNNING': info['detail']['mileage'].replace(' کیلومتر', '').replace(',', ''),
                'FUEL': info['detail']['fuel'],
                'GEAR': info['detail']['transmission'],
                'YEAR': info['detail']['year'],
                'MODEL': info['detail']['title'],
                'BODY_CONDITION': info['detail']['body_status'],
-               'PRICE': info['price']['price']}
+               'PRICE': info['price']['price'].replace(',', '')}
         dataSet.append(car)
     return dataSet
 
 
-def save(data: list):
+connection = sqlite3.connect("cars_db.sqlite")
+cursor = connection.cursor()
+
+
+def save(cars: list):
+    counter = 0
+    print('Conecting to Database\nSaving Started...')
     cursor.execute('DELETE FROM INFO_CAR;')
+    connection.commit()
     query = '''INSERT INTO INFO_CAR (COLOR, KM_RUNNING, FUEL, GEAR, YEAR, MODEL, BODY_CONDITION, PRICE)
-                VALUES (%s,%i,%i,%i,%i,%s,%i,%i);'''
-    cursor.execute(query, ())
+                VALUES (?,?,?,?,?,?,?,?);'''
+    for car in cars:
+        value = (car['COLOR'], car['KM_RUNNING'], car['FUEL'], car['GEAR'],
+                 car['YEAR'], car['MODEL'], car['BODY_CONDITION'], car['PRICE'])
+        cursor.execute(query, value)
+        connection.commit()
+        counter += 1
+    print(f'{counter} items saved in database')
